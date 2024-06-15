119d525cda7aabe3c0a5dedc2169cf6644d40792
GOATAFI <projectexecutiveofpm@gmail.com>
2024-06-15 23:26:12 +0600

    variable declaration

--- test.php
+++ test.php
@@ -1,4 +1,18 @@
 <?php
-echo "Hello";
+$name = "Nahid Parvez";
+$age = 24;
+//10.35 e charge theke khulsi
+
+$word = "age";
+
+echo $$word;
+echo "\n";
+
+echo 'Hello ' . $name;
+echo "\n";
+echo "Hello $name";
+echo "\n";
+
+echo "Hello {$name} , you are {$age} years old  ";
 
 ?>
