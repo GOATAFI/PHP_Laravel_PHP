9e591efd108bdfbba07d015e2b304a794f9a9ddb
GOATAFI <projectexecutiveofpm@gmail.com>
2024-06-19 13:22:54 +0600

    continue and break

--- loop.php
+++ loop.php
@@ -4,3 +4,19 @@ for ($i = 0; $i<100;$i++){
     echo $i;
     echo PHP_EOL;
 }
+
+for($i = 20; $i<30 ;$i++){
+    if($i <27){
+        continue;
+    }
+    echo $i;
+    echo PHP_EOL; 
+}
+
+for($i = 20;$i<30;++$i){
+    if($i == 25){
+        break;
+    }
+    echo $i;
+    echo PHP_EOL;
+}
