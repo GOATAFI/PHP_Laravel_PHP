cd335201844fff634f32240d87ea0cb4fc5ec280
GOATAFI <projectexecutiveofpm@gmail.com>
2024-06-22 09:58:12 +0600

    array manipulation with a helper function

--- arrayManipulation.php
+++ arrayManipulation.php
@@ -1,41 +1,38 @@
 <?php
 
-$students = ['Nahid','Setu','Afsana','Mafi'];
+$students = ['Nahid', 'Setu', 'Afsana', 'Mafi'];
 
-
-$n = count($students);
-for($i=0;$i<$n;$i++){
-    echo $students[$i]."\n";
+// Function to print the array
+function printArray($array) {
+    foreach ($array as $element) {
+        echo $element . "\n";
+    }
 }
 
-echo PHP_EOL;
-//----------- array_push to push element in the last index of the array ---------------
-array_push($students,'Parvez');
-$n = count($students);
-for($i=0;$i<$n;$i++){
-    echo $students[$i]."\n";
-}
+printArray($students);
 
 echo PHP_EOL;
-// ----------array_pop to pop the last element from the array -------------
+
+// ----------- array_push to push element in the last index of the array ---------------
+array_push($students, 'Parvez');
+printArray($students);
+
+echo PHP_EOL;
+
+// ---------- array_pop to pop the last element from the array -------------
 array_pop($students);
-$n = count($students);
-for($i=0;$i<$n;$i++){
-    echo $students[$i]."\n";
-}
+printArray($students);
 
 echo PHP_EOL;
+
 // ----------- array_shift to delete the first element of the array ------------
 array_shift($students);
-$n = count($students);
-for($i=0;$i<$n;$i++){
-    echo $students[$i]."\n";
-}
+printArray($students);
 
 echo PHP_EOL;
-//----------- array_unshift to insert element at the top of the array -----------
-array_unshift($students,"Maafffiii");
-$n = count($students);
-for($i=0;$i<$n;$i++){
-    echo $students[$i]."\n";
-}
+
+// ----------- array_unshift to insert element at the top of the array -----------
+array_unshift($students, "Maafffiii");
+printArray($students);
+
+?>
