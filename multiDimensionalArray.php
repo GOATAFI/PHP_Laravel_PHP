21aba3bd942d530343851e6e9c6ffb16319023f2
GOATAFI <projectexecutiveofpm@gmail.com>
2024-06-22 12:32:27 +0600

    multi dimen array

--- multiDimensionalArray.php
+++ multiDimensionalArray.php
@@ -13,3 +13,12 @@ $sample = [
 
 print_r($sample);
 echo $sample['test']['test-again'][2];
+
+echo PHP_EOL;
+$sample2 = [
+    [1,2,3,4],
+    [5,6,7,8],
+    [11,22,33,[10,20,30]]
+];
+print_r($sample2);
+echo $sample2[2][3][2];
