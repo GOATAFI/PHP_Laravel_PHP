445aae741c268599e047fd74c0653f0826d17767
GOATAFI <projectexecutiveofpm@gmail.com>
2024-06-24 13:14:10 +0600

    some comments

--- arraySplice.php
+++ arraySplice.php
@@ -6,3 +6,7 @@ $newFruits = array_splice($fruits,1,3,$moreFruits);
 
 print_r($newFruits);
 print_r($fruits);
+
+// array slice method e original array te kono provab pore na kintu splice method e original array te provab pore 
+//splice method e notun array ou insert kora jay
+
