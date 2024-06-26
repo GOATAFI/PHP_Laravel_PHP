be04bf65589a065f29e9302b6c2a50fe03025a39
GOATAFI <projectexecutiveofpm@gmail.com>
2024-06-27 00:11:48 +0600

    array_intersect_assoc for associative array

--- arrayElementErMilOmil.php
+++ arrayElementErMilOmil.php
@@ -5,3 +5,9 @@ $number2 = [3,5,4,6,11,23];
 
 $common = array_intersect($number1,$number2);
 print_r($common);
+
+$fruits1= ['a' =>'apple','b' => 'banana', 'o' => 'orange','p' => 'alta','k' => 'kathal','m' => 'mango', 'd' => 'dates'];
+$fruits2 = ['a' =>'apple','c' => 'banana', 'o' => 'orange','p' => 'alta','j' => 'kathal','m' => 'mango', 'd' => 'dates'];
+
+$commonFruit = array_intersect_assoc($fruits1,$fruits2);
+print_r($commonFruit);
