a20beefd3eee5dfce0b8a275a68a250d303dc012
GOATAFI <projectexecutiveofpm@gmail.com>
2024-06-29 01:00:13 +0600

    array_reduce

---
+++ arrayReduce.php
@@ -0,0 +1,12 @@
+<?php
+$numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+function sum($oldValue, $newValue)
+{
+    if ($newValue % 2 == 0) {
+        return $oldValue + $newValue;
+    }
+    return $oldValue;
+}
+
+$sum = array_reduce($numbers, 'sum');
+echo $sum;
--- walk_map_filter.php
+++ walk_map_filter.php
@@ -5,31 +5,34 @@ function square($n)
     printf("Square of %d is %d \n", $n, $n * $n);
 }
 
-array_walk($numbers,'square');
+array_walk($numbers, 'square');
 
 //kintu array_walk() abar return korte pare nah , er jonno lage array_map()
 
-function cube($n){
-    return $n * $n *$n;
+function cube($n)
+{
+    return $n * $n * $n;
 }
 
-$newArray = array_map('cube',$numbers);
+$newArray = array_map('cube', $numbers);
 
 print_r($newArray);
 
-function even($n) {
-    return $n %2 ==0;
+function even($n)
+{
+    return $n % 2 == 0;
 }
-$evenNumbers = array_filter($numbers,'even');
-$evenNumbers2 = array_map('even',$numbers);
+$evenNumbers = array_filter($numbers, 'even');
+$evenNumbers2 = array_map('even', $numbers);
 
 print_r($evenNumbers);
 print_r($evenNumbers2);
 
-$names = ['mafi','kalu','sakib','modon'];
-function name($n){
+$names = ['mafi', 'kalu', 'sakib', 'modon'];
+function name($n)
+{
     return $n[0] == 'm';
 }
 
-$namesStartedWithM = array_filter($names,'name');
+$namesStartedWithM = array_filter($names, 'name');
 print_r($namesStartedWithM);
