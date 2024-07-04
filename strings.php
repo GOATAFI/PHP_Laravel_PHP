209bb2027a70a6a5b4f6b18b8bf2d7e8ca4c7b82
GOATAFI <projectexecutiveofpm@gmail.com>
2024-07-05 00:38:34 +0600

    heredoc

--- strings.php
+++ strings.php
@@ -4,3 +4,16 @@ $name = "Mafi";
 echo 'My name is $name \n'; //parse korbe na , echo hoye jabe jemon ase temon e , $name e show korbe , Mafi ar show korbe na cz single quote er jonno , double quote dile abar ei problem thakbe nah
 echo PHP_EOL;
 echo "My name is $name \n"; //Now okkk
+
+//-------- heredoc -------
+$heredoc = <<<EOD
+Hiiiiii
+This is {$name} speaking \n \n
+
+
+
+
+
+EOD;
+
+echo $heredoc;
