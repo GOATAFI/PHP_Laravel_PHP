598f7158b498847c1a654e74f34e1ae61db43734
GOATAFI <projectexecutiveofpm@gmail.com>
2024-07-04 22:54:47 +0600

    single and double quotes

---
+++ strings.php
@@ -0,0 +1,6 @@
+<?php
+$name = "Mafi";
+
+echo 'My name is $name \n'; //parse korbe na , echo hoye jabe jemon ase temon e , $name e show korbe , Mafi ar show korbe na cz single quote er jonno , double quote dile abar ei problem thakbe nah
+echo PHP_EOL;
+echo "My name is $name \n"; //Now okkk
