e4f37d6eb2ce39a3f1740a44ba043fe27314e647
GOATAFI <projectexecutiveofpm@gmail.com>
2024-07-06 10:03:42 +0600

    string reverse

---
+++ stringReverse.php
@@ -0,0 +1,16 @@
+<?php
+$string = "Hello World";
+$length = strlen($string);
+for($i = $length - 1; $i >= 0; $i--) {
+    echo $string[$i];
+}
+
+echo PHP_EOL;
+
+for($i=1;$i<=$length;$i++){
+    echo $string[$i*-1];
+}
+
+echo PHP_EOL;
+
+echo strrev($string);
